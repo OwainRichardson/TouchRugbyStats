94785f1b04fe8919f439fc39dd9be666d57cd427
Owain Richardson <Owain.Richardson@purplebricks.com>
2022-10-19 08:59:31 +0100

    Add failing test case to ask for help with

--- src/Tests/PlayerFrameTests.test.js
+++ src/Tests/PlayerFrameTests.test.js
@@ -1,5 +1,5 @@
 import { render, fireEvent, screen } from '@testing-library/react';
-import MatchContextProvider from '../Contexts/MatchContext';
+import MatchContextProvider, { MatchContext } from '../Contexts/MatchContext';
 import MatchContainer from "../Components/MatchContainer";
 import Timer from "../Components/Timer";
 import Teams from "../Components/Teams";
@@ -21,7 +21,30 @@ test("Try scored for home team adds 1 to home team score", () => {
     const scorer = screen.getByText('Owain');
     fireEvent.click(scorer);
 
-
     const homeScore = document.querySelector('#home-score');
     expect(homeScore.textContent).toBe('1');
+});
+
+test("Try scored for home team adds 1 to home team score in context", () => {
+
+    let context = {
+        homeScore: 0,
+        possession: 'home'
+    };
+
+    render(
+        <MatchContext.Provider value={context}>
+            <Timer />
+            <Teams />
+            <MatchContainer />    
+        </MatchContext.Provider>
+    );
+
+    const tryScoredButton = screen.getByText('Try scored');
+    fireEvent.click(tryScoredButton);
+
+    const scorer = screen.getByText('Owain');
+    fireEvent.click(scorer);
+
+    expect(context.homeScore).toBe(1);
 });
