3269b9407bec9b55ebe813dc59a31fb4f5f1b24a
Owain Richardson <owain.richardson@gmail.com>
2023-03-10 21:44:22 +0000

    Update styles

--- touch-rugby-stats/components/Teams/index.jsx
+++ touch-rugby-stats/components/Teams/index.jsx
@@ -1,13 +1,14 @@
 import './index.module.css';
-import { useMatchContext } from '../../Contexts/MatchContext';
+import { useMatchContext } from '../../contexts/MatchContext';
 import { useEffect } from 'react';
+import styles from './index.module.css';
 
 const Teams = () => {
     const { homeScore, awayScore, possession, homeTeam, awayTeam } = useMatchContext();
 
     useEffect(() => {
-        let homeTeamName = document.querySelector('.team-name.home');
-        let awayTeamName = document.querySelector('.team-name.away');
+        let homeTeamName = document.querySelector('#team-name-home');
+        let awayTeamName = document.querySelector('#team-name-away');
 
         if (possession === 'home') {
             homeTeamName.classList.add('in-possession');
@@ -19,11 +20,11 @@ const Teams = () => {
     }, [possession]);
 
     return (
-        <div className="team-container">
-            <div className="team home wildcats"><span className="team-name home">{homeTeam}</span></div>
-            <div className="score home wildcats home-score" id="home-score">{homeScore}</div>
-            <div className="score away saxons">{awayScore}</div>
-            <div className="team away saxons"><span className="team-name away">{awayTeam}</span></div>
+        <div className={styles['team-container']}>
+            <div className={`${styles['team']} ${styles['home']} ${styles['wildcats']}`}><span className={`${styles['team-name']} ${styles['home']}`} id="team-name-home">{homeTeam}</span></div>
+            <div className={`${styles['score']} ${styles['home']} ${styles['wildcats']} ${styles['home-score']}`}>{homeScore}</div>
+            <div className={`${styles['score']} ${styles['away']} ${styles['saxons']}`}>{awayScore}</div>
+            <div className={`${styles['team']} ${styles['away']} ${styles['saxons']}`}><span className={`${styles['team-name']} ${styles['away']}`} id="team-name-away">{awayTeam}</span></div>
         </div>
     );
 };
